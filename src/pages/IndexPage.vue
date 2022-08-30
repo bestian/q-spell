49c95198899d0dcb0a2f8ec8e930a70d4e31506b
Bestian Tang 小巴 <bestian@gmail.com>
2022-08-30 09:21:32 +0800

    css

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,41 @@
 <template>
   <q-page class="flex flex-center">
-    <table class="styled-table container">
+
+
+    <div class="pad thin-only">
+      <h3>光明真言</h3>
+      <p>𑖌𑖼𑖀𑖦𑖺𑖑𑖪𑖹𑖨𑖺𑖓𑖡<br/>𑖦𑖮𑖯𑖦𑖲𑖟𑖿𑖨𑖯𑖦𑖜𑖰𑖢𑖟𑖿𑖦<br/>𑖕𑖿𑖪𑖯𑖩𑖢𑖿𑖨𑖪𑖨𑖿𑖝𑖿𑖝𑖧𑖮𑗝𑖽
+          </p>
+      <p>oṃ amogha vairocana mahā-mudra maṇi-padma jvala pravarttaya hūṃ</p>
+
+      <p>嗡 阿模嘎 百羅佳那 嘛哈母德喇 嘛尼 叭德嘛 及乏拉 缽喇乏爾達亞 吽</p>
+
+      <p>讚歎大日如來的無瑕遍照。把寶珠、蓮花和光芒交還給我。 </p>
+
+      <h3>如意寶輪王陀羅尼</h3>
+
+      <p>Namo ratna-trayāya
+Nama Āryāvalokiteśvarāya bodhisattvāya mahāsattvāya mahākāruṇikāya
+tadyathā
+Oṃ cakravarti cintāmaṇi mahāpadme ru ru tiṣṭhat jvala ākarṣāya hūṃ phaṭ svāhā
+Oṃ padma cintāmaṇi jvala hūm
+Oṃ varada padme hūm</p>
+
+      <p>南謨佛馱耶　南謨達摩耶　南謨僧伽耶
+南謨觀自在菩薩摩訶薩　具大悲心者
+怛姪他
+唵斫羯羅伐底　震多末尼謨訶　鉢蹬謎　嚕嚕嚕嚕　底瑟他　篅攞痾羯利沙也　吽發莎訶
+菴鉢踏摩　震多末尼　篅攞吽
+菴跋剌陀　鉢亶謎吽</p>
+
+      <p>皈依三寶佛法僧，<br/>禮敬聖者觀自在，<br/>大悲心覺有情者，<br/>
+真言宣說眾功德；<br/>運轉神變殊勝遇，<br/>轉輪聖王世間貴，<br/>
+蓮華成就最勝尊，<br/>普願皆滿悉如意；<br/>此咒一出諸震動，<br/>
+即時句召光明境，<br/>法螺遍響立變現，<br/>圓滿成就功德具。</p>
+
+    </div>
+
+    <table class="styled-table container fat-only">
       <thead>
         <tr><th>咒文</th><th>悉曇文</th><th>羅馬拼音</th><th>漢語音譯</th><th class="wide">意譯</th></tr>
       </thead>
